4caa623764475e578b621da07aeb9a546ea1f739
rahul7011 <champrahul01@gmail.com>
2021-05-31 13:36:56 +0530

    refactor

--- src/Containers/PostList.js
+++ src/Containers/PostList.js
@@ -6,7 +6,7 @@ import Loadingicon from "../Components/Loadingicon";
 import MessageLoader from "../Components/MessageLoader";
 import useFetch from "../helpers/hooks";
 import ReactMarkdown from "react-markdown";
-import Truncate from 'react-truncate';
+import Truncate from "react-truncate";
 const PostList = () => {
   const { data, loading, error } = useFetch(api.posts.list);
   return (
@@ -19,19 +19,21 @@ const PostList = () => {
         {data?.map((post) => {
           return (
             <Item key={post.id}>
-              <Item.Image size="small" src={post.thumbnail} />
-
+                <Item.Image size="small" src={post.thumbnail} className="img"/>
               <Item.Content>
                 <NavLink to={`/posts/${post.slug}`}>
                   <Item.Header as="h3">{post.title}</Item.Header>
                 </NavLink>
-                <>
-
-                <Truncate lines={3} ellipsis={<span>... <a href={`/posts/${post.slug}`}>Read more</a></span>}>
+                <Truncate
+                  lines={3}
+                  ellipsis={
+                    <span>
+                      ... <a href={`/posts/${post.slug}`}>Read more</a>
+                    </span>
+                  }
+                >
                   <ReactMarkdown>{post.content}</ReactMarkdown>
                 </Truncate>
-
-                </>
               </Item.Content>
             </Item>
           );
